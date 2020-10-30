8134b2bef440560261cf2744a05242179a7de4cc
Pritam Majumder <ptmmajumder630@gmail.com>
2020-10-30 15:01:46 +0530

    Updated request.php

--- request.php
+++ request.php
@@ -1,8 +1,24 @@
-<?php 
+<?php
+ 
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');   
+    }
 
-$msg = $_POST['postmsg'];
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-$chatbot = exec("python chatbot.py $msg");
-echo $chatbot;
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
 
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+    $msg = $_POST['postmsg'];
+
+    $chatbot = exec("python chatbot.py $msg");
+    echo $chatbot;
 ?>
